5c82e113d1cc487b02883cc992465316cceae0be
NatalieBlack <natalievictoriablack@gmail.com>
2019-04-22 15:40:09 -0400

    Ability to drag member cards between two lists

---
+++ src/WaitingArea/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Member from '../Member';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const WaitingArea = ({members, getListStyle, getItemStyle}) => {
+    return (
+                    <Droppable droppableId="droppable">
+                        {(provided, snapshot) => (
+                            <div
+                                ref={provided.innerRef}
+                                style={getListStyle(snapshot.isDraggingOver)}>
+                                {members.map((item, index) => (
+                                    <Draggable
+                                        key={item.id}
+                                        draggableId={item.id}
+                                        index={index}>
+                                        {(provided, snapshot) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                style={getItemStyle(
+                                                    snapshot.isDragging,
+                                                    provided.draggableProps.style
+                                                )}>
+                                                <Member member={item.content} />
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+    )
+}
+
+export default WaitingArea;
